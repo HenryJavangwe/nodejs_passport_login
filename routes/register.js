5593315cf688aa06117b2b3b8160bd82568c1ca1
Henry Javangwe <henrysparks1@gmail.com>
2020-05-22 19:52:54 +0200

    created a local database(a json file to save our data of our new users), changed the model of the data so that we'll have a unique id created for each new users registration. to be able to handle requests, we required the package request and also we'll need to access the database and make posts request to it, so we require the database by creating a new variable and pointingt it to the loaction of the db --> const user = require('./../db.json'). created a post route in the register, and passed a function redirect that'll take us to the index after creating a new user.

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const user = require('./../db.json')
+const request =require ('request')
 
 /* GET register page. */
 router.get('/', function(req, res, next) {
   res.render('register');
+});
+
+router.post('/', function(req, res, next) {
+    // set a new dynamic id
+  let newId = Number(++user.usersDb.length);
+  console.log("Creating " + req.body.Username + " on the new id of: " + newId);
+  // make a post request to our database and update the id
+  request({
+    url: "http://localhost:3000/usersDb",
+    method: "POST",
+    form: {
+      id: Number(newId),
+      Username: req.body.Username,
+      email: req.body.email,
+      Password: req.body.Password
+    }
+  }, 
+  function(error, response, body) {
+    // send a response message
+    console.log("The error is: ", error);
+    console.log("The body:", body);
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
